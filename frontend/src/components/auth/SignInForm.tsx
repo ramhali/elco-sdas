e11aefba7d433842123f32d68ede0c27b2454690
ramhali <mcafable1@up.edu.ph>
2026-08-28 11:58:31 +0800

    Add frontend to repository

---
+++ frontend/src/components/auth/SignInForm.tsx
@@ -0,0 +1,150 @@
+import { useState } from "react";
+import { Link } from "react-router";
+import { ChevronLeftIcon, EyeCloseIcon, EyeIcon } from "../../icons";
+import { login } from "../../api/apiAuthentication";
+
+import Label from "../form/Label";
+import Input from "../form/input/InputField";
+// import Checkbox from "../form/input/Checkbox";
+import Button from "../ui/button/Button";
+import Alert from "../ui/alert/Alert";
+
+export default function SignInForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  // const [isChecked, setIsChecked] = useState(false);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setError("");
+
+    try {
+      const data = await login({ username, password });
+
+      console.log("Login successful:", data);
+
+      window.location.href = "/";
+      // Example:
+      // localStorage.setItem("access_token", data.access);
+
+      // Navigate to dashboard
+      // navigate("/");
+    } catch (error) {
+      setError(error instanceof Error ? error.message : "Invalid username or password.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col flex-1">
+      <div className="w-full max-w-md pt-10 mx-auto">
+        <Link
+          to="/"
+          className="inline-flex items-center text-sm text-gray-500 transition-colors hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+        >
+          <ChevronLeftIcon className="size-5" />
+          Back to dashboard
+        </Link>
+      </div>
+      <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
+        <div>
+          <div className="mb-5 sm:mb-8">
+            <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
+              Sign In
+            </h1>
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              Enter your username and password to sign in!
+            </p>
+          </div>
+          <div>
+            {error && (
+              <div className="mb-5">
+                <Alert
+                  variant="error"
+                  title="Login failed"
+                  message={error}
+                  showLink={false}
+                />
+              </div>
+            )}
+            <form onSubmit={handleSubmit}>
+              <div className="space-y-6">
+                <div>
+                  <Label>
+                    Username <span className="text-error-500">*</span>{" "}
+                  </Label>
+                  <Input
+                    placeholder="Enter your username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <Label>
+                    Password <span className="text-error-500">*</span>{" "}
+                  </Label>
+                  <div className="relative">
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Enter your password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <span
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
+                    >
+                      {showPassword ? (
+                        <EyeIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+                      ) : (
+                        <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+                      )}
+                    </span>
+                  </div>
+                </div>
+                <div className="flex items-center justify-between">
+                  {/* <div className="flex items-center gap-3">
+                    <Checkbox checked={isChecked} onChange={setIsChecked} />
+                    <span className="block font-normal text-gray-700 text-theme-sm dark:text-gray-400">
+                      Keep me logged in
+                    </span>
+                  </div> */}
+                  <Link
+                    to="/reset-password"
+                    className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
+                <div>
+                  <Button className="w-full" size="sm">
+                    Sign in
+                  </Button>
+                </div>
+              </div>
+            </form>
+
+            <div className="mt-5">
+              <p className="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start">
+                Don't have an account yet? {""}
+                <Link
+                  to="/signup"
+                  className="text-brand-500 hover:text-brand-600 dark:text-brand-400"
+                >
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
